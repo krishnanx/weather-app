f2a52a9f92cde213bd8c9eee14589addc1410c3d
krishnanx <abhishekkrishnan2006@gmail.com>
2024-07-03 12:59:09 +0530

    Form is created and city is received,now weather should be displayed

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React,{useState,useEffect, useContext} from 'react'
+import Zoom from 'react-reveal/Zoom';
 import Typewriter from 'typewriter-effect';
 import './HomePage.css'
 import clear from "../Pics/clear.jpg"
@@ -10,7 +11,35 @@ import windy from "../Pics/windy.jpg"
 import foggy from "../Pics/foggy.jpg"
 import humid from "../Pics/humid.jpg"
 import clear2 from "../Pics/clear-2.jpg"
+import {weather} from "../Wcontext"
+import { fetchData } from '../Weather';
 const HomePage = () => {
+    
+    const [value, setValue] = useState("");
+    const [city,setCity] = useContext(weather);
+    const [showElement, setShowElement] = useState(false);
+    useEffect(() => {
+        const handleScroll = () => {
+          const scrollThreshold =700;
+          console.log(window.scrollY);
+          if (window.scrollY > scrollThreshold) {
+            setShowElement(true);
+
+          } else {
+            setShowElement(false);
+          }
+        };
+      
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+      }, []);
+       const HandleClick=(value)=>{
+        const weatherData = fetchData(value);
+        console.log(weatherData)
+
+       }
   return (
     <div className='Home' /*style={{height:'500px'}}*/>
         <div className='container' >
@@ -53,10 +82,40 @@ const HomePage = () => {
                 </div>
             </div>
         </div>
-        <div className='container-2'>
-            <img src={clear2} className='pic'></img>
+       
+        <div className={`container-2 ${showElement ? 'visible' : ''}`}>
             
+            <div className='pic-div'>
+                <form className='form'>
+                    <input 
+                    type="text" 
+                    className='textbox' 
+                    placeholder='Enter City' 
+                    onChange={(e)=>{
+                        setValue(e.target.value)
+                        console.log(e.target.value)
+                    }}
+                    value={value}
+                    /> 
+                    <button 
+                    className="button-17" 
+                    role="button"
+                    onClick={(e)=>{
+                        e.preventDefault();
+                        HandleClick(value);
+                    }}
+                    >Search
+                    </button>
+                </form>
+                <img src={clear2} className='pic'></img>
+            </div>
+           
+           
+                
         </div>
+ 
+       
+        
         
         
          
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,28 +1,48 @@
-import React,{useEffect, useState} from 'react'
+import React,{useContext, useEffect, useState} from 'react'
 const Url='https://www.weatherapi.com/docs/#'
 const API_KEY = '0fd2b9d39c994c0890c64412240207';
 import axios from 'axios';
-const Weather = () => {
-    const city="Paris"
-    useEffect(()=>{
-        const fetchData=async()=>{
-            try {
-                const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=0fd2b9d39c994c0890c64412240207&q=${city}&aqi=yes`)
-                  
-                if(!response){
-                  setError(true)
-                }
+import { weather } from './Wcontext';
+export const fetchData=async(myCity)=>{
+  try {
+      const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=0fd2b9d39c994c0890c64412240207&q=${myCity}&aqi=yes`)
+        
+      if(!response){
+        setError(true)
+      }
 
-                const json = response.data;
-                console.log(json)
+      const json = response.data;
+      console.log(json)
+      return response.data;
+      
+    } catch (error) {
+      console.log("LOL")
+      console.log(error.message)
+      console.log("LOL")
+    }
+  
+}
+const Weather = () => {
+    const [city,setCity]=useContext(weather)
+    // useEffect(()=>{
+    //     const fetchData=async()=>{
+    //         try {
+    //             const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=0fd2b9d39c994c0890c64412240207&q=${city}&aqi=yes`)
+                  
+    //             if(!response){
+    //               setError(true)
+    //             }
+
+    //             const json = response.data;
+    //             console.log(json)
                 
-              } catch (error) {
-                console.log(error.message)
-              }
+    //           } catch (error) {
+    //             console.log(error.message)
+    //           }
             
-        }
-        fetchData();
-       },[])
+    //     }
+    //     fetchData();
+    //    },)
        
   return (
     <div>
